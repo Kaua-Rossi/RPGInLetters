e07c662351f531ce12fc934785ab504626557132
Kaua-Rossi <kauarossipersonal@gmail.com>
2026-01-30 23:39:39 -0300

    refactor(Item): remove nullable symbol from Name and Description fields

--- RPGInLetters/Models/Item.cs
+++ RPGInLetters/Models/Item.cs
@@ -3,8 +3,8 @@
     internal class Item
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
         public enum ItemType
         {
             Weapon,
